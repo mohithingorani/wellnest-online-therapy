4691e5769a1cb3f84d3bf4ac53dc82749cb39979
Mohit Hingorani <Mohithingorani2003@gmail.com>
2026-04-23 21:13:56 +0530

    sign in responsive

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,10 +1,10 @@
 export default function Signin() {
-    return <div className="h-screen  p-4 w-full font-nunito flex justify-center">
-        <div className="bg-[#235C61] max-w-7xl w-full h-full p-6 grid grid-cols-2 rounded-4xl">
-            <div className="relative overflow-hidden rounded-bl-4xl">
+    return <div className="h-screen  lg:p-4 w-full font-nunito flex justify-center">
+        <div className="bg-[#235C61] max-w-7xl w-full h-full md:p-4 lg:p-6 grid lg:grid-cols-2 lg:rounded-4xl">
+            <div className="relative overflow-hidden hidden lg:inline-block rounded-bl-4xl">
                 <img width={600} className="absolute -bottom-2 -left-2" src="/signup.png" alt="WellNest"/>
             </div>
-            <div className="bg-white rounded-3xl p-6 flex flex-col justify-between h-full">
+            <div className="bg-white md:rounded-2xl lg:rounded-3xl p-6 flex flex-col justify-between h-full">
 
                 {/* TOP */}
                 <div className="flex justify-between">
@@ -22,10 +22,10 @@ export default function Signin() {
                 
                 {/* FORM */}
                 <div className="px-6 flex flex-col gap-4">
-                    <div className="text-5xl tracking-wide">Sign In</div>
-                     <input type="text" placeholder="Email or Username" className="text-lg px-6 py-4  rounded-full border-2 border-black/20 w-full" />
-                   <input type="text" placeholder="Password" className="text-lg px-6 py-4  rounded-full border-2 border-black/20 w-full" />
-                   <div  className="flex justify-center gap-4 items-center text-white text-xl px-6 py-4  rounded-full border-2 border-[#235C61] bg-[#235C61] w-full" >
+                    <div className="text-3xl md:text-5xl tracking-wide">Sign In</div>
+                     <input type="text" placeholder="Email or Username" className="text-sm md:text-lg px-6 py-4  rounded-full border-2 border-black/20 w-full" />
+                   <input type="text" placeholder="Password" className="text-sm md:text-lg px-6 py-4  rounded-full border-2 border-black/20 w-full" />
+                   <div  className="flex justify-center gap-4 items-center text-white md:text-xl px-3 py-2 md:px-6 md:py-4  rounded-full border-2 border-[#235C61] bg-[#235C61] w-full" >
                     <img src="/go.svg" alt="Enter" />
                     <div>
                         Sign In
@@ -35,7 +35,7 @@ export default function Signin() {
 
                 </div>
 
-                <div>
+                <div className="text-sm">
                     © 2026 WellNest. All rights reserved.
                 </div>
 
