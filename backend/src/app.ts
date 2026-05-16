0b5740173e197534a7375991adae48638f388704
Mohit Hingorani <Mohithingorani2003@gmail.com>
2026-05-17 01:08:52 +0530

    fixed cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://wellnest-therapy.vercel.app"],
     credentials: true,
   }),
 );
